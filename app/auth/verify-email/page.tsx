75506ec93b27c3f84d48e8f0d54359175b40b920
Philip Boen <philipboen001@gmail.com>
2024-03-15 17:34:37 +0300

    implement email verification

---
+++ app/auth/verify-email/page.tsx
@@ -0,0 +1,7 @@
+import { VerificationForm } from "@/components/auth/verification-form";
+
+export default function Page() {
+    return (
+        <VerificationForm />
+    )
+}
---
+++ components/auth/verification-form.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import Link from "next/link";
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { verification } from "@/lib/actions/verification";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+
+export const VerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get("token");
+
+  const handleSubmit = useCallback(() => {
+    if (!token) {
+      setError("Token is missing!");
+      return;
+    }
+
+    verification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
+  }, [token]);
+
+  useEffect(() => {
+    handleSubmit();
+  }, [handleSubmit]);
+
+  return (
+    <div className="flex h-[70vh] items-center justify-center">
+      <Card className="w-full max-w-[500px] px-4 shadow-md max-sm:rounded-none max-sm:border-none max-sm:shadow-none">
+        <CardContent className="mt-6">
+          <p className="h2-bold text-center">Confirming your verification</p>
+          <div className="my-4 flex w-full items-center justify-center">
+            {!success && !error && <BeatLoader />}
+            <FormError message={error} />
+            <FormSuccess message={success} />
+          </div>
+        </CardContent>
+        <CardFooter className="flex items-center justify-center">
+          <Button variant="link" className="px-2" asChild>
+            <Link href="/auth/login">Back to Login</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
