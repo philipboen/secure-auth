b3e58f67f7f0f817654fbed66e9e7d98d230db62
Philip Boen <philipboen001@gmail.com>
2024-03-21 01:49:50 +0300

    modify resend domain

--- lib/services/mail.ts
+++ lib/services/mail.ts
@@ -8,7 +8,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `${domain}/auth/verify-email?token=${token}`;
 
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'onboarding@secure-auth.systems',
         to: email,
         subject: 'Verify Your Email',
         html: `<p>Click <a href="${confirmLink}">here</a> to verify your email.</p>`
@@ -19,7 +19,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
     const resetLink = `${domain}/auth/new-password?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "onboarding@secure-auth.systems",
         to: email,
         subject: "Reset Your Password",
         html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`
@@ -28,7 +28,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "onboarding@secure-auth.systems",
         to: email,
         subject: "Your Two-Factor Code",
         html: `<p>Your two-factor code is: <strong>${token}</strong></p>`
